95aad53894d2f95d2d67a087ac85af30d1302315
Ehud-Karavani <ehud.karavani@ibm.com>
2023-06-05 11:25:46 +0300

    Add docstrings

--- causallib/contrib/orthogonal_regression/orthogonal_regression.py
+++ causallib/contrib/orthogonal_regression/orthogonal_regression.py
@@ -23,23 +23,80 @@ class OrthogonalRegression:
         covariate_models: Dict[Hashable, Union[Regressor, Classifier]],
         id_col: Hashable,
         time_col: Hashable,
-        lag=1,
+        lag: int = 1,
         pool_time=False,
     ):
+        """Orthogonal regression for time-varying effect estimation.
+        Orthogonal regression is a method to transform time-varying data
+        in order to make valid effect estimation in the presence of
+        treatment-confounder feedback.
+
+        The method works by modeling the time-varying covariates at each
+         time point using the covariates and treatment assignment from previous
+         time-points and then residualizing them.
+
+        This model implements a generic variation of the algorithm presented in
+        Causal Inference with Orthogonalized Regression Adjustment: Taming the Phantom
+        by Bates et al. 2022, https://arxiv.org/abs/2201.13451
+
+        Args:
+            covariate_models: A mapping between covariate names and an estimator.
+                It is up to the user to match binary covariates with "classification
+                models" (implementing `predict_proba()`) and match continuous covariates
+                with regressors (implementing `predict()`).
+                Covariates existing in the data and not here will not be modeled and
+                residualized (tranformed), i.e., will be kept as is. This fits covariates
+                that are either baseline covariates or may not be part of a
+                treatment-confounder feedback loop.
+            id_col: The name of the column specifying an observation's ID.
+            time_col: The name of the column specifying the time
+                      (data in assumed to be in a person-time "long" format).
+            lag: Number of time-steps history to use when modeling a covariate at time `t`.
+                 Uses all time-points possible until `t-lag`, e.g., if lag=2, at `t=2` it
+                 will only use `t=1` but at `t=3` it will use both `t=1` and `t=2` and at
+                 `t=4` it will again use `t=2` and `t=3`.
+            pool_time: Whether to pool together different time-points into a single model.
+                       NotImplemented, currently IGNORED.
+        """
         self.covariate_models = covariate_models
         self.id_col = id_col
         self.time_col = time_col
         self.lag = lag
-        self.pool_time = pool_time
+        # self.pool_time = pool_time  # TODO: add a time-pooled model?
 
-    def fit(self, X, a, y=None):
+    def fit(
+        self,
+        X: pd.DataFrame,
+        a: pd.DataFrame,
+        y=None,
+    ):
+        """Fits an Orthogonal Regression transformer.
+
+        Args:
+            X: Time-varying covariates in person-time ("long") format,
+               with ID column name specified in `self.id_col` and
+               time column name specified in `self.time_col`.
+               Baseline covariates can be specified by first being duplicated
+               at every time point, and second having a time_point preceding
+               the time-varying time-points. For example, if time-varying covariates
+               start at `t=1`, then baseline covairates should start at `t=0`.
+               Column names should match the keys in `self.covariate_models`.
+               Time-varying covariates that shouldn't be modelled can exist as
+               columns in `X` but not be specified as keys in `self.covariate_models`.
+            a: Time-varying treatment assignment in person-time ("long") format,
+               with ID column name specified in `self.id_col` and
+               time column name specified in `self.time_col`.
+            y:
+
+        Returns:
+            OrthogonalRegression
+        """
         # TODO: check for match between self.covariate_models.keys() and X.columns?
         models = defaultdict(dict)  # TODO: should index by either time/cov first or even by tuple?
         Xa = X.merge(a, on=["id", "t"], how="outer")
         # a-priori merging saves some duplicated wrangling, but creates edge cases (e.g., all-null predictors/targets)
         time_points = sorted(Xa[self.time_col].unique())
         for time_point in time_points:
-            # TODO: add a time-pooled model?
             for covariate in self.covariate_models.keys():
                 cur_y = self._get_target(Xa, covariate, time_point)
                 if cur_y.isna().all():
@@ -61,6 +118,9 @@ class OrthogonalRegression:
         return self
 
     def _get_predictors(self, Xa, time_point, index, covariate=None):
+        """Extracts the predictors matrix for a given time.
+        `index` is required in case an empty set of predictors is defined and
+        an intercept is required to be generated manually."""
         cur_time_span = max((Xa[self.time_col]).min(), time_point - self.lag)
         cur_Xa = Xa.loc[Xa[self.time_col].between(cur_time_span, time_point - 1)]
         # cur_Xa = cur_Xa[covariate]  # TODO: model by just the covariate? predictor set per covariate?
@@ -71,6 +131,7 @@ class OrthogonalRegression:
         return cur_Xa
 
     def _get_target(self, Xa, covariate, time_point):
+        """Extracts the prediction outcome at a given time."""
         # cur_y = Xa.loc[Xa[self.time_col] == time_point, [self.id_col, covariate]]
         # cur_y = cur_y.set_index(self.id_col)[covariate]  # Remove column dimension
         cur_y = (
@@ -80,6 +141,9 @@ class OrthogonalRegression:
         return cur_y
 
     def _long_to_wide(self, cur_Xa, dropna=True):
+        """Transforms long person-time format to wide format indexed by person.
+        `dropna` is used to drop entirely empty columns
+        (no information for some covariate at specific time)."""
         cur_Xa = cur_Xa.pivot(index=self.id_col, columns=self.time_col)
         cur_Xa.columns = [f"{c[0]}__{c[1]}" for c in cur_Xa.columns.to_flat_index()]
         if dropna:
@@ -91,6 +155,8 @@ class OrthogonalRegression:
         return cur_Xa
 
     def _orthogonalize(self, X, a, y=None):
+        """Main logic for orthogonalizing the time-varying covariates.
+        Returns `X` and `a` combined in a long-format."""
         Xa = X.merge(a, on=["id", "t"], how="outer")
         res = Xa.copy()
         time_points = sorted(Xa[self.time_col].unique())
@@ -114,7 +180,29 @@ class OrthogonalRegression:
                 res.loc[res[self.time_col] == time, covariate] = cur_resid
         return res
 
-    def transform(self, X, a, y=None):
+    def transform(
+        self,
+        X,
+        a,
+        y=None
+    ):
+        """Orthogonalizes the data.
+        Data is returned in wide-format, indexed by person ID,
+        ready to be modelled against a non-time-varying outcome `y`.
+
+        Args:
+            X: Time-varying covariates in person-time ("long") format,
+               with ID column name specified in `self.id_col` and
+               time column name specified in `self.time_col`,
+               matching the input specification in `fit()`.
+            a: Time-varying treatment assignment in person-time ("long") format,
+               with ID column name specified in `self.id_col` and
+               time column name specified in `self.time_col`.
+            y:
+
+        Returns:
+            pd.DataFrame: wide-format orthogonalized data.
+        """
         res = self._orthogonalize(X, a, y)
         res = self._long_to_wide(res)
         return res
